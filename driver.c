78c8489cddfd14fc16a702bc05f28ae888851802
Nabil Brice <nabilbrice@yahoo.co.uk>
2024-05-13 11:32:03 +0100

    fixed bug with loading subtable and tidied up

--- driver.c
+++ driver.c
@@ -1,56 +1,62 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-const int TABLE_SIZE_E = 10;
-const int TABLE_SIZE_P = 2;
+// currently the information about dimensions of the data
+// have to be given here:
+// giving the incorrect dimensions results in a garbled table
+const int TABLE_DIM_E = 9;
+const int TABLE_DIM_P = 2;
+const int NUMBER_OF_TABLES = 2;
+const int TABLE_DIMS = TABLE_DIM_E * TABLE_DIM_P * NUMBER_OF_TABLES;
 
+// T_ijk with j being adjacent in memory
 float *get_table_value(float *table, int i, int j, int k) {
-  int stride_k = TABLE_SIZE_E * TABLE_SIZE_P;
-  int stride_i = TABLE_SIZE_P;
+  int stride_k = TABLE_DIM_E * TABLE_DIM_P;
+  int stride_i = TABLE_DIM_P;
   return &table[stride_k * k + stride_i * i + j];
 }
 
-void readtable(FILE *file, float *table, int k) {
-  char buff[2048];
+void load_subtable(float *table, FILE *file, int k) {
+  char buff[1024];
 
-  int i;
-
-  i = 0;
-  while (fgets(buff, sizeof(buff), file)) {
+  int i = 0;
+  // condition to capture end of file and end of table
+  // while loop is needed to bypass the comments
+  while (fgets(buff, sizeof(buff), file)&&(i<TABLE_DIM_E)) {
     if (buff[0]=='#') {
       continue;
-    }
+    };
     sscanf(buff, "%f %f", get_table_value(table, i, 0, k), get_table_value(table, i, 1, k));
     printf("subtable: %f %f \n", *get_table_value(table, i, 0, k), *get_table_value(table, i, 1, k));
     i++;
   };
+  printf("finished loading table %d\n", k);
 }
 
 
 int main() {
-
-  // the table is actually bigger than necessary in entries
-  float table[2][TABLE_SIZE_E][TABLE_SIZE_P];
-
   FILE *file;
-  char buff[2048]; // maximum sized buffer
+  int i, k;
 
-  int i, j, k, l;
-
-  float *boxed_table = malloc( TABLE_SIZE_E * TABLE_SIZE_P * 2 * sizeof(boxed_table));
+  // the table stored in memory:
+  float *boxed_table = malloc(TABLE_DIMS * sizeof(boxed_table));
 
   file = fopen("example.dat", "r");
-  j = 0;
-  for (int j=0; (j < 2); j++) {
-    readtable(file, boxed_table, j);
+  for (int k=0; (k < NUMBER_OF_TABLES); k++) {
+    load_subtable(boxed_table, file, k);
   };
 
-  k = 4;
-  l = 1;
-  printf("For k = %d in table %d\n", k, l+1);
-  printf("Energy[k] = %f, Scattering cross-section: %f\n", *get_table_value(boxed_table, k, 0, l), 
-    *get_table_value(boxed_table, k, 1, l));
-
+  i = 0;
+  k = 0;
+  printf("For i = %d in table %d\n", i, k+1);
+  printf("Energy[k] = %f, Scattering cross-section: %f\n", *get_table_value(boxed_table, i, 0, k), 
+    *get_table_value(boxed_table, i, 1, k));
+  i = 1;
+  k = 1;
+  printf("For i = %d in table %d\n", i, k+1);
+  printf("Energy[k] = %f, Scattering cross-section: %f\n", *get_table_value(boxed_table, i, 0, k), 
+    *get_table_value(boxed_table, i, 1, k));
 
   free(boxed_table);
   return 0;
